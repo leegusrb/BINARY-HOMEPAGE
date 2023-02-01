1cc555adec5097da635af186c0fc4aa7a00d0d8a
Lee Hyeon Gyu <jeffsep24@gmail.com>
2023-02-01 13:57:20 +0900

    backend & login page

---
+++ back-end/src/main/java/com/binary/homepage/domain/Member.java
@@ -0,0 +1,26 @@
+package com.binary.homepage.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    private Integer studentId;
+
+    private String password;
+
+    private Integer generation;
+}
---
+++ back-end/src/main/java/com/binary/homepage/service/MemberService.java
@@ -0,0 +1,41 @@
+package com.binary.homepage.service;
+
+import com.binary.homepage.domain.Member;
+import com.binary.homepage.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원 전체 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 회원 1명 조회
+     */
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
+    /**
+     * 회원 수정
+     */
+    @Transactional
+    public void update(Long id, String name) {
+        Member member = memberRepository.findOne(id);
+        member.setName(name);
+    }
+
+}
